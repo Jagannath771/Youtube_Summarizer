8b45189eb67aa1105ae7123df2130c8735144199
Jagannath Sai <44517623+Jagannath771@users.noreply.github.com>
2024-09-10 12:14:31 -0400

    claim_generator.py

--- claims/claim_generator.py
+++ claims/claim_generator.py
@@ -25,10 +25,10 @@ def health_video_check(prompt, summary_text):
 
 def extract_transript_details(video_id):
     try:
-        proxy = {
-            'http': 'socks5://bxvojwdh:sivol2wm9ey1@38.154.227.167:5868'
-         }
-        transcript_text=YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy)
+        # proxy = {
+        #     'http': 'socks5://bxvojwdh:sivol2wm9ey1@38.154.227.167:5868'
+        #  }
+        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
         transcript=""
         for i in transcript_text:
             transcript+=" "+i["text"]
